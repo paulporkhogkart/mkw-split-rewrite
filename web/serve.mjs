5b00e7752d6080388bd0ce52d79e83f09843a8f0
Paul Pork <paulporkhogkart@gmail.com>
2026-06-19 00:49:48 +1000

    bump version

--- web/serve.mjs
+++ web/serve.mjs
@@ -11,6 +11,7 @@ const TYPES = {
   ".html": "text/html; charset=utf-8", ".js": "text/javascript; charset=utf-8",
   ".css": "text/css; charset=utf-8", ".json": "application/json; charset=utf-8",
   ".svg": "image/svg+xml", ".png": "image/png", ".jpg": "image/jpeg", ".jpeg": "image/jpeg",
+  ".gif": "image/gif",
   ".webp": "image/webp", ".ico": "image/x-icon", ".woff2": "font/woff2", ".woff": "font/woff",
   ".map": "application/json", ".txt": "text/plain; charset=utf-8",
 };
